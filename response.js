aa658b4e63ca598c4abd1249e2c357291d8f450f
wagle2 <archmage.maple@gmail.com>
2019-05-02 18:43:54 +0900

    버스초기화

--- response.js
+++ response.js
@@ -14,7 +14,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
         cut = String.fromCharCode(8237).repeat(500)  
         r = { replier: replier, msg: msg, sender: sender, room: room};
 
-
+        광주버스정류장불러오기(r);
         if (room == 'test' || room == '시립대 봇제작방' || room == '고딩' || room == '정인') {
             if(msg =="!로딩" ){
                 reload(r);
