7a0ecf2182935a2b20fccce16929a7ac1bd59bd5
wagle2 <archmage.maple@gmail.com>
2019-05-02 22:48:23 +0900

    음

--- response.js
+++ response.js
@@ -103,13 +103,14 @@ function 버스현재위치(r){
     busstopId = r.msg.split(" ")[1];
     busstopInfo = org.jsoup.Jsoup.connect("http://api.gwangju.go.kr/json/arriveInfo?ServiceKey=BknKnKlcOt5e3xllE%2Fboca5kw2Dzmqwm2lNf7XEmAporlHM7JPggxLbS8GgtoSO6%2FcLjBJKOgOMSH6Bmt4EUlw%3D%3D&serviceKey=&BUSSTOP_ID="+busstopId).get()
     busstopInfoJson = JSON.parse(busstopInfo.select("body").text());
-    busNum = busstopInfoJson.BUSSTOP_LIST.length
     test="          [버스정보알림]\n------------------------------------\n"
     for(i=0;i<busNum;i++){
         test += (busstopInfoJson.BUSSTOP_LIST[i].LINE_NAME.toString() + "  ("+ busstopInfoJson.BUSSTOP_LIST[i].REMAIN_MIN.toString() + "분) (" + busstopInfoJson.BUSSTOP_LIST[i].BUSSTOP_NAME.toString()  + ")\n")
     }
+    r.replier.reply("흠섹");
     r.replier.reply(test);
 }
+
 function 광주버스정류장받아오기(r){
     busstopId = r.msg.split(" ")[1];
     url = "http://api.gwangju.go.kr/json/stationInfo?ServiceKey=BknKnKlcOt5e3xllE%2Fboca5kw2Dzmqwm2lNf7XEmAporlHM7JPggxLbS8GgtoSO6%2FcLjBJKOgOMSH6Bmt4EUlw%3D%3D&serviceKey="
