79210c0b04caa80b11ac2f4d00dccd44eae6b33f
wagle2 <archmage.maple@gmail.com>
2019-05-02 16:51:57 +0900

    dd

--- response.js
+++ response.js
@@ -65,7 +65,7 @@ function 고딩방(r) {
 function 광주버스(r){
 
     var1 = org.jsoup.Jsoup.connect("http://m.gwangju.go.kr/bus/api/stationArrive.do")
-            .cookies("searchStation","%5B%7B%22KWD%22%3A%22646%22%2C%22DT%22%3A%2220190502%22%7D%5D")
+            .cookies("searchStation","D")
             .get();
 }
 
