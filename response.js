cc25d9cd229d6d9b3cbf3f7bda843c435466950e
wagle2 <archmage.maple@gmail.com>
2019-05-02 17:09:59 +0900

    스플릿

--- response.js
+++ response.js
@@ -63,61 +63,8 @@ function 고딩방(r) {
 }
 
 function 광주버스(r){
-
-    var1 = org.jsoup.Jsoup.connect("http://m.gwangju.go.kr/bus/api/stationArrive.do")
-            .cookies("searchStation","D")
-            .get();
-}
-
-function notice(r){
-	try{
-		if(Flag.get('cookie1', 'test') == 0 || Flag.get('cookie2', 'test') == 0){
-			var cookie1 = org.jsoup.Jsoup.connect("http://www.knfb1377.or.kr/bbs/login.php?url=%2Fhtml%2Fmain.html")
-			.method(org.jsoup.Connection.Method.GET).execute().cookies();
-
-			var cookie2 = org.jsoup.Jsoup.connect("https://www.knfb1377.or.kr:9001/bbs/login_check.php").cookies(cookie1)
-			.data("mb_id","tyfb1377").data("mb_password","1q2w3e4r").data("x","30").data("y","30")
-			.method(org.jsoup.Connection.Method.POST).execute().cookies();
-	
-			Flag.set('cookie1', 'test', cookie1);
-			Flag.set('cookie2', 'test', cookie2);
-		}
-		
-		var doc = org.jsoup.Jsoup.connect("http://www.knfb1377.or.kr/bbs/board.php?bo_table=10_01")
-	    .cookies(Flag.get('cookie2', 'test')).cookies(Flag.get('cookie1', 'test')).get().select('tbody');
-
-	    var temptext = doc.select("tr.num").toArray().map(v=>"번호:"+v.select("td.num").get(0).text()+"   날짜:"+v.select("td.date").text()+"\n"+v.select("td.title>a").first().ownText());
-	    var text = [];
-	    var count = r.msg.split(" ")[1];
-	    var lastnum = doc.select("tr.num").get(14).select("td.num").get(0).text();
-	    
-	    if(lastnum-1<count){
-	    	var firstnum = doc.select("tr.num").get(0).select("td.num").get(0).text();
-	        var wantnum = firstnum-count;
-	    	var docnum = doc.select("tr.num").get(wantnum).select("td.num").get(0).text();
-	    	var doctitle = doc.select("tr.num").select("a:first-child").get(wantnum).ownText();
-	    	var doclink = doc.select("tr.num").select("a:first-child").get(wantnum).attr("abs:href");
-	    	
-	    	var subdoc = org.jsoup.Jsoup.connect(doclink).cookies(Flag.get('cookie2', 'test')).cookies(Flag.get('cookie1', 'test')).get();
-	    	
-	    	var text = String(subdoc.select("div.content").toArray()[0]).replace(/<br>/g, '\n').replace(/(<([^>]+)>)/g, "").replace(/&nbsp;/g, ' ').trim().replace(/^ +/gm,"").replace(/\n\n\n/g, '\n').replace(/\n\n\n/g, '\n');
-	    	var repl = subdoc.select("div.comment_area").eachText().toArray().join('\n\n').replace(/관리자 /g, "").replace(/답변 /g, "\n").replace(/수정 삭제 /g, '');
-	    	
-	    	r.replier.reply(docnum+" : "+doctitle+"\n----------------------------------\n"+es+text+"\n----------------------------------\n"+repl+"\n----------------------------------\n"+doclink);
-	    }else if(0<count&&count<16){
-	    	for(i=0;i<count;i++){
-	    		text.push(temptext[i]);
-	    	}
-	    	r.replier.reply(text.join("\n\n"));
-	    }else{
-	    	for(i=0;i<5;i++){
-	    		text.push(temptext[i]);
-	    	}
-	    	r.replier.reply(text.join("\n\n"));
-	    }
-	}catch(e){
-		Api.replyRoom('test',e+"\n"+e.stack);
-		}
+    text = r.msg.split(" ")[1];
+    r.replier.reply(text)
 }
 
  function reload(r) {
