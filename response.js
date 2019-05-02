92089d4cde3354db0070be6f6b2e1bd9e3f0a511
wagle2 <archmage.maple@gmail.com>
2019-05-02 17:15:36 +0900

    버스스톱

--- response.js
+++ response.js
@@ -63,8 +63,11 @@ function 고딩방(r) {
 }
 
 function 광주버스(r){
-    text = r.msg.split(" ")[1];
-    r.replier.reply(text)
+    busstopId = r.msg.split(" ")[1];
+    busstopInfo = org.jsoup.Jsoup.connect("http://api.gwangju.go.kr/json/arriveInfo?ServiceKey=BknKnKlcOt5e3xllE%2Fboca5kw2Dzmqwm2lNf7XEmAporlHM7JPggxLbS8GgtoSO6%2FcLjBJKOgOMSH6Bmt4EUlw%3D%3D&serviceKey=&BUSSTOP_ID="+busstopId).get()
+
+    r.replier.reply(busstopId+"번 정류장 버스 정보\n"+busstopInfo);
+
 }
 
  function reload(r) {
