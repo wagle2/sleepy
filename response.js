704ebae1f3be9690f6ccc356a7d6a29c7febdced
wagle2 <archmage.maple@gmail.com>
2019-05-03 09:45:19 +0900

    trim

--- response.js
+++ response.js
@@ -203,8 +203,10 @@ function 버스현재위치(r,busStopName,next_busStopName){
     for(i=0;i<busNum;i++){
         result += (busstopInfoJson.BUSSTOP_LIST[i].LINE_NAME.toString() + "  ("+ busstopInfoJson.BUSSTOP_LIST[i].REMAIN_MIN.toString() + "분) (" + busstopInfoJson.BUSSTOP_LIST[i].BUSSTOP_NAME.toString()  + ")\n")
     }
-    r.replier.reply(result);
+    r.replier.reply(result.trim());
+    //trim은 문자의 양끝 공백 제거
 }
+
 
 function 광주버스정류장받아오기(r){
     busstopId = r.msg.split(" ")[1];
