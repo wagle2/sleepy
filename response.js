11976606d31304e8f401d61a0ed986eb11cf60c4
wagle2 <archmage.maple@gmail.com>
2019-05-04 17:28:30 +0900

    ㅇㅁㅇㄹ

--- response.js
+++ response.js
@@ -290,7 +290,7 @@ weather = {
         var str = ' 시　　날씨　기온 강수 습도 풍량\n';
         for(var i in baseTodayWeather){
             var repeatStr = baseTodayWeather[i];
-            str += " "+ String(repeatStr.select("hour").text()).extension("0",2) + "　";
+            str += String(repeatStr.select("hour").text()).extension("0",2) + "　";
             str += String(repeatStr.select("wfKor").text()).replace(/\s/g,"").extensionRight(한글공백,5);
             str += String(repeatStr.select("temp").text()).slice(0,-2).extension("0",2)+ "　";
             str += String(repeatStr.select("pop").text()).extensionRight(" ",2) + "　";
