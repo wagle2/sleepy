5755e82facb475a3aa999d25b2f284ac4f4639f2
wagle2 <archmage.maple@gmail.com>
2019-05-04 17:54:22 +0900

    d

--- response.js
+++ response.js
@@ -300,7 +300,7 @@ weather = {
             str += String(repeatStr.select("reh").text()) + "　";
             str += repeatStr.select("ws").text().substring(0,3)+"\n";
         }
-        str.substr(0,s.length-1);
+        str.substr(0,str.length-1);
         return str;
     }
 }
