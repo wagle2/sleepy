ab2eb3d73787f882f9bc444a2523ca8fc65da060
wagle2 <archmage.maple@gmail.com>
2019-05-06 19:44:24 +0900

    dd

--- response.js
+++ response.js
@@ -376,7 +376,7 @@ weather = {
         this.str += "(해)" + location + "\n　→ " 
                         + nowTime + "\n------------------------------------\n"
                         + "시　　날씨　기온 습도 최저 최고\n" 
-                        + new Date().getHours() + "　" + nowWeather + "　" + nowTemp + "　" + nowTemp + "　"+ todayLowTemp + "　"+ todayHighTemp + "\n"
+                        + new Date().getHours() + "　" + nowWeather.replace(/\(.*?\)/g,"") + "　" + nowTemp + "　" + nowTemp + "　"+ todayLowTemp + "　"+ todayHighTemp + "\n"
                         + "------------------------------------\n";;
 
         var baseLink = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=" + areaCode;
