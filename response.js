7d1b816248450cbd7ea5434864386fcb4ead9abc
wagle2 <archmage.maple@gmail.com>
2019-05-06 19:07:59 +0900

    dd

--- response.js
+++ response.js
@@ -324,26 +324,19 @@ weather_test = {
             var todayHighTemp = weatherSoup.select("div > div:nth-child(1) > div > div.card.card_now > div.weather_set > div.set.set_text > div > span.day_high > em").text()
             
 
-            var times = weatherSoup.select("div > div > div.card.card_graph > div.graph_content > div > div > table > tbody > tr:nth-child(7)").text().split(" ")
-            var weatherlength = times.length-1;
-            var todaysWeather = (String(weatherSoup.select("div > div > div.card.card_graph > div.graph_content > div > div > table > tbody > tr:nth-child(2) div")).split("\n"));
-            var todaysTemp = weatherSoup.select("div > div > div.card.card_graph > div._cnWtrHourlyChartData > div:nth-child(1)").text().split(",")
-            var todaysRain = weatherSoup.select("div > div > div.card.card_graph > div._cnWtrHourlyChartData > div:nth-child(2)").text().split(",")
-            var todaysWind = weatherSoup.select("div > div > div.card.card_graph > div._cnWtrHourlyChartData > div:nth-child(3)").text().split(",")
-            var todaysReh = weatherSoup.select("div > div > div.card.card_graph > div._cnWtrHourlyChartData > div:nth-child(4)").text().split(",")
+            //var times = weatherSoup.select("div > div > div.card.card_graph > div.graph_content > div > div > table > tbody > tr:nth-child(7)").text().split(" ")
+            //var weatherlength = times.length-1;
+            //var todaysWeather = (String(weatherSoup.select("div > div > div.card.card_graph > div.graph_content > div > div > table > tbody > tr:nth-child(2) div")).split("\n"));
+            //var todaysTemp = weatherSoup.select("div > div > div.card.card_graph > div._cnWtrHourlyChartData > div:nth-child(1)").text().split(",")
+            //var todaysRain = weatherSoup.select("div > div > div.card.card_graph > div._cnWtrHourlyChartData > div:nth-child(2)").text().split(",")
+            //var todaysWind = weatherSoup.select("div > div > div.card.card_graph > div._cnWtrHourlyChartData > div:nth-child(3)").text().split(",")
+            //var todaysReh = weatherSoup.select("div > div > div.card.card_graph > div._cnWtrHourlyChartData > div:nth-child(4)").text().split(",")
 
             this.resultStr = "";
             this.resultStr += "(해)" + location + "\n　→ " 
                             + nowTime + "\n------------------------------------\n"
-                            + "날씨　　　기온 습도 최저 최고\n" ;
-            for (var i=0;i<weatherlength;i++){
-                this.resultStr += times[i]+ "　"
-                                + todaysWeather[3*i+1] + "　"
-                                + todaysTemp[i]+ "　"
-                                + todaysRain[i]+ "　"
-                                + todaysWind[i] + "　"
-                                + todaysReh[i]+"\n" 
-            }
+                            + "날씨　　기온　습도　최저　최고" 
+                            + nowWeather + "　" + nowTemp + "　" + nowTemp + "　"+ todayLowTemp + "　"+ todayHighTemp + " ";
             return this.resultStr;
         }
     },
