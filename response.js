5e2768574b65b80b9d7976659bd7782c46bf0995
wagle2 <archmage.maple@gmail.com>
2019-05-10 19:57:57 +0900

    asdf

--- response.js
+++ response.js
@@ -507,13 +507,14 @@ reinforceGame = function(r){
     } else {
         //items 배열을 모두 돌아가면서 체크한다.
         for(i in items){
+            r.repier(senderCode+" "+itemName);
             //같은 sender와 같은 아이템 이름인 경우에
-            if(items[i].senderCode === senderCode && items[i].name === itemName){
+            if(items[i].senderCode == senderCode && items[i].name == itemName){
                 items[i].reinforced(r);
             } 
             // Case2 : 아이템이 없을 때
             else {
-                r.replier.reply("아이템을 생성합니다.1");
+                r.replier.reply("아이템을 생성합니다.");
                 items.push(new item(r,itemName,senderCode));
             }
         }
