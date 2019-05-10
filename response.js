f24ce995865f8c2826b9239d0469d51dd116260e
wagle2 <archmage.maple@gmail.com>
2019-05-10 19:56:14 +0900

    sdfa

--- response.js
+++ response.js
@@ -497,6 +497,14 @@ reinforceGame = function(r){
     var sender = new java.lang.String(r.sender);
     var senderCode = sender.hashCode();
     
+    // items에 아무것도 없을 때
+    if(items.length === 0){
+        r.replier.reply("아이템을 생성합니다.");
+        r.replier.reply(items.length);
+        items.push(new item(r,itemName,senderCode));
+        return;
+    // items에 뭔가가 있을 때
+    } else {
         //items 배열을 모두 돌아가면서 체크한다.
         for(i in items){
             //같은 sender와 같은 아이템 이름인 경우에
@@ -510,6 +518,7 @@ reinforceGame = function(r){
             }
         }
     }
+}
 
 
 // Date.prototype
