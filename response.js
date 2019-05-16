d363fd6a80124ba0309e88ecf3d0dafe2a6d0886
wagle2 <archmage.maple@gmail.com>
2019-05-16 16:24:19 +0900

    ㅁㄴㅇㄹ

--- response.js
+++ response.js
@@ -169,6 +169,8 @@ var CHO = T.register("chosun",()=>{
             Api.replyRoom(r.room,msg)}}}
         choNotice(r)
         choNotice2(r)
+        r.replier.reply("잘 작동중")
+        java.lang.Thread.sleep(2*1000);
 		}
 }).start();
 
