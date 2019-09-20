3614052ecd1a7cd569faa862aade621f064cad45
wagle2 <archmage.maple@gmail.com>
2019-09-20 20:56:33 +0900

    asdf

--- response.js
+++ response.js
@@ -7,73 +7,140 @@ var K = require("KBManager.js");
 숫자공백 = String.fromCharCode(8199);
 투명공백 = String.fromCharCode(8237);
 function response(room, msg, sender, isGroupChat, replier, imageDB) {
-    /** @param {String} room - 방 이름
-      * @param {String} msg - 메세지 내용
-      * @param {String} sender - 발신자 이름
-      * @param {Boolean} isGroupChat - 단체채팅 여부
-      * @param {Object} replier - 세션 캐싱 답장 메소드 객체
-      * @param {Object} imageDB - 프로필 이미지와 수신된 이미지 캐싱 객체
-      * @method imageDB.getImage() - 수신된 이미지가 있을 경우 Base64 인코딩 되어있는 JPEG 이미지 반환, 기본 값 null
-      * @method imageDB.getProfileImage() - Base64 인코딩 되어있는 JPEG 프로필 이미지 반환, 기본 값 null
-      * @method replier.reply("문자열") - 메시지가 도착한 방에 답장을 보내는 메소드 */
-    r = { replier: replier, msg: msg, sender: sender, room: room};
-    if (room == 'test' || room == '시립대 봇제작방') {
-        if(msg =="^로딩" ){
-            reload(r);
-            return;
-        }
+  /** @param {String} room - 방 이름
+   * @param {String} msg - 메세지 내용
+   * @param {String} sender - 발신자 이름
+   * @param {Boolean} isGroupChat - 단체채팅 여부
+   * @param {Object} replier - 세션 캐싱 답장 메소드 객체
+   * @param {Object} imageDB - 프로필 이미지와 수신된 이미지 캐싱 객체
+   * @method imageDB.getImage() - 수신된 이미지가 있을 경우 Base64 인코딩 되어있는 JPEG 이미지 반환, 기본 값 null
+   * @method imageDB.getProfileImage() - Base64 인코딩 되어있는 JPEG 프로필 이미지 반환, 기본 값 null
+   * @method replier.reply("문자열") - 메시지가 도착한 방에 답장을 보내는 메소드 */
+  r = { replier: replier, msg: msg, sender: sender, room: room };
+  if (room == "test" || room == "시립대 봇제작방") {
+    if (msg == "^로딩") {
+      reload(r);
+      return;
     }
-    if(msg == "어흥"){
-        r.replier.reply("애옹애옹");
-    } else if(msg == "애옹"){
-        r.replier.reply("어흐으응");
+  }
+  if (msg == "어흥") {
+    r.replier.reply("애옹애옹");
+  } else if (msg == "애옹") {
+    r.replier.reply("어흐으응");
+  }
+  if (msg[0] === "^" && room === "시립대 봇제작방") {
+    try {
+      replier.reply(eval(msg.substring(1)));
+    } catch (e) {
+      replier.reply(e);
     }
-    if (msg[0] === "^" && room === '시립대 봇제작방') {
-        try{
-            replier.reply(eval(msg.substring(1)));
-        } catch(e){
-            replier.reply(e);
-        }
+  }
+}
+
+if(msg[0] === "^한일"){
+    replier.reply(한일(msg.substring(4)));
+}
+
+function 한일(r) {
+    const text = r.msg.split(" ")[1];
+    r.replier.reply(
+      한일1(text) +
+        "\n\n" +
+        일한1(한일1(text)) +
+        "\n\n" +
+        한일1(일한1(한일1(text)))
+    );
+  }
+  
+  function 한일1(r) {
+    const apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+    const text = r;
+    const userAgent =
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21";
+    return JSON.parse(
+      org.jsoup.Jsoup.connect(apiURL)
+        .userAgent(userAgent)
+        .header("X-Naver-Client-Id", "ra4TWI7i1c4UkntvakEg")
+        .header("X-Naver-Client-Secret", "OnUVOoJMYp")
+        .data({ source: "ko", target: "ja", text: text })
+        .ignoreHttpErrors(true)
+        .followRedirects(true)
+        .ignoreContentType(true)
+        .post()
+        .select("body")
+        .text()
+    ).message.result.translatedText;
+  }
+  
+  function 일한1(r) {
+    const apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+    const text = r;
+    const userAgent =
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21";
+    return JSON.parse(
+      org.jsoup.Jsoup.connect(apiURL)
+        .userAgent(userAgent)
+        .header("X-Naver-Client-Id", "ra4TWI7i1c4UkntvakEg")
+        .header("X-Naver-Client-Secret", "OnUVOoJMYp")
+        .data({ source: "ja", target: "ko", text: text })
+        .ignoreHttpErrors(true)
+        .followRedirects(true)
+        .ignoreContentType(true)
+        .post()
+        .select("body")
+        .text()
+    ).message.result.translatedText;
+  }
+
+fileName = () => new Date().getTime().toString();
+
+function save(text) {
+  const file = "storage/emulated/0/kbot/" + fileName() + ".txt";
+  const filedir = new java.io.File(file);
+  var bw = new java.io.BufferedWriter(new java.io.FileWriter(filedir));
+  bw.write(text);
+  bw.close();
+  return 0;
+}
+
+function reload(r) {
+  if (r.sender == "잠만보") {
+    reloadcheck = 1;
+    var Timer = new Date();
+    file = "storage/emulated/0/kbot/response.js";
+    checksum = org.jsoup.Jsoup.connect(
+      "https://github.com/wagle2/sleepy/commits/master"
+    )
+      .get()
+      .select("div.repository-content>a")
+      .attr("href")
+      .split("commit/")[1];
+    conn = new java.net.URL(
+      "https://raw.githubusercontent.com/wagle2/sleepy/" +
+        checksum +
+        "/response.js"
+    ).openConnection();
+    br = new java.io.BufferedReader(
+      new java.io.InputStreamReader(conn.getInputStream())
+    );
+    str = "";
+    tmp = null;
+    while ((tmp = br.readLine()) != null) {
+      str += tmp + "\n";
     }
+    var filedir = new java.io.File(file);
+    var bw = new java.io.BufferedWriter(new java.io.FileWriter(filedir));
+    bw.write(str.toString());
+    bw.close();
+    var time = (new Date() - Timer) / 1000;
+    r.replier.reply("파일저장 완료 / " + time + "s\n" + new Date());
+    Api.reload();
+    var time = (new Date() - Timer) / 1000;
+    reloadcheck = 0;
+    r.replier.reply("reloading 완료 / " + time + "s\n" + new Date());
+  }
+}
 
- }
-
-fileName = () => new Date().getTime().toString()
-
- function save(text){
-     const file = "storage/emulated/0/kbot/"+fileName()+".txt";
-     const filedir = new java.io.File(file);
-     var bw = new java.io.BufferedWriter(new java.io.FileWriter(filedir));
-        bw.write(text);
-        bw.close();
- }
-
- function reload(r) {
-    if(r.sender=='잠만보'){
-        reloadcheck = 1;
-        var Timer = new Date();
-        file = "storage/emulated/0/kbot/response.js";
-        checksum = org.jsoup.Jsoup.connect("https://github.com/wagle2/sleepy/commits/master").get().select("div.repository-content>a").attr("href").split('commit/')[1];
-        conn = new java.net.URL("https://raw.githubusercontent.com/wagle2/sleepy/"+checksum+"/response.js").openConnection();
-        br = new java.io.BufferedReader(new java.io.InputStreamReader(conn.getInputStream()));
-        str = "";
-        tmp = null;
-        while ((tmp = br.readLine()) != null) {
-            str += tmp + "\n";
-        }
-        var filedir = new java.io.File(file);
-        var bw = new java.io.BufferedWriter(new java.io.FileWriter(filedir));
-        bw.write(str.toString());
-        bw.close();
-        var time = (new Date() - Timer) / 1000;
-        r.replier.reply("파일저장 완료 / " + time + "s\n" + new Date() );
-        Api.reload();
-        var time = (new Date() - Timer) / 1000;
-        reloadcheck = 0;
-        r.replier.reply("reloading 완료 / " + time + "s\n" + new Date());
-    }
- }
- 
 //이 아래 6가지 메소드는 스크립트 액티비티에서 사용하는 메소드들
 function onCreate(savedInstanceState, activity) {}
 function onStart(activity) {}
