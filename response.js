1c2854b3b8e4bde0cf6e631e67ae8f9c24a43a6d
wagle2 <archmage.maple@gmail.com>
2019-09-21 01:44:20 +0900

    ㅁㄴㄹ

--- response.js
+++ response.js
@@ -31,12 +31,12 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
   if(msg =="/채팅분석") {
     const messages = D.selectForArray("Chats","chat").join(",");
     const messages2 = messages.replace(/[\{\}\[\]\/?.;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g,"");
+    r.replier.reply("http://wagle.dlinkddns.com/stat.png");
     org.jsoup.Jsoup.connect(
         "http://wagle.dlinkddns.com:5000/messages/"+encodeURI(messages2)
       )
         .get().timeout(5000)
         .text()
-    r.replier.reply("http://wagle.dlinkddns.com/stat.png");
     }
   if (msg == "어흥") {
     r.replier.reply("애옹애옹");
