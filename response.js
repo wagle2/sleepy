687d0244bf46b54a5b6bc7e2b08efd2903acd4cc
wagle2 <archmage.maple@gmail.com>
2019-09-20 21:19:59 +0900

    ㅁㄴㅇㄹ

--- response.js
+++ response.js
@@ -17,7 +17,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
    * @method imageDB.getProfileImage() - Base64 인코딩 되어있는 JPEG 프로필 이미지 반환, 기본 값 null
    * @method replier.reply("문자열") - 메시지가 도착한 방에 답장을 보내는 메소드 */
   r = { replier: replier, msg: msg, sender: sender, room: room };
-  if (room == "test" || room == "시립대 봇제작방") {
+  if (1) {
     if (msg == "^로딩") {
       reload(r);
       return;
