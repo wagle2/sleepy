9865e50dd9aac45b290cb158dc54e97aa015f7a9
wagle2 <archmage.maple@gmail.com>
2019-09-27 20:11:39 +0900

    asdf

--- response.js
+++ response.js
@@ -233,7 +233,7 @@ function weather(loc) {
   const temp_min = currentWeather.main.temp_min;
   const temp_max = currentWeather.main.temp_max;
   const clouds = currentWeather.clouds.all;
-  r.replier.reply(loc + "날씨\n" + main + " / 기온 : " + temp + "ºC\n최저 : " + temp_min + " 최고 : "+ temp_max);
+  r.replier.reply(loc + "날씨\n" + main + " /  기온 : " + Math.round(temp) + "º\n최저 : " + temp_min + "º 최고 : "+ temp_max + "º");
 }
 
 function getLoc(loc) {
