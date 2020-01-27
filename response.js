62d1043d91d2f1a909bb0a9589525306814ce2d5
wagle2 <archmage.maple@gmail.com>
2020-01-27 18:06:26 +0900

    Update response.js

--- response.js
+++ response.js
@@ -74,9 +74,9 @@ function monitor(room, sender, checkFunc, extractFunc, time) {
 
   var q = new java.util.concurrent.LinkedBlockingQueue();
   AnswerSet.put(q, q); //대기 큐에 추가
-  Api.replyRoom(room, "ㅇㅇㅇ.\n");
   var thr = new java.lang.Thread(
     new java.lang.Runnable(function() {
+      Api.replyRoom(room, "ㅇㅇㅇ.\n");
       try {
         while (true) {
           var tmp = q.take(); //메세지 큐 소비
