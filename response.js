26c59495c9d5d7fe7db8d6105ed384532004be95
wagle2 <archmage.maple@gmail.com>
2020-01-27 17:48:27 +0900

    Update response.js

--- response.js
+++ response.js
@@ -51,7 +51,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
 }
 function monitor(room, sender, checkFunc, extractFunc, time) {
   var returner = "";
-
+  Api.replyRoom(room, "ㅇㅇㅇ.\n3");
   var q = new java.util.concurrent.LinkedBlockingQueue();
   AnswerSet.put(q, q); //대기 큐에 추가
   var thr = new java.lang.Thread(
